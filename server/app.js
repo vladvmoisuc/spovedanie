c1329a52a78dc7a101cc1d7c6aed2e6468ef7654
Vlad Moisuc <moisuc.vlad1@gmail.com>
2020-02-02 22:37:36 +0200

    Prepare server and client side for deploy.

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
@@ -6,17 +7,23 @@ const routes = require("./routes");
 const db = require("./db");
 const logger = require("./middlewares/logger");
 const debug = require("debug")("spovedanie");
+const path = require("path");
 
 db.connect();
 
 // Middlewares
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use("/api/", routes);
 
-const port = config.get("port");
+// Serve files
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
+const port = config.get("port");
 app.listen(port, function() {
   debug(`Server started to listen on port ${port}.`);
 });
